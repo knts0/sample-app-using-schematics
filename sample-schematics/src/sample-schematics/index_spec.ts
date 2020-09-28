c2c936c1207596025b6272a869401cb0c7398371
Kana Otawara <shimmer22.0204@gmail.com>
2020-09-29 01:41:19 +0900

    add test specs (more variation and exception)

--- sample-schematics/src/sample-schematics/index_spec.ts
+++ sample-schematics/src/sample-schematics/index_spec.ts
@@ -7,8 +7,9 @@ const collectionPath = path.join(__dirname, '../collection.json');
 
 
 describe('sample-schematics', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+
+  it('should create component files', () => {
     const tree = runner.runSchematic('sample-schematics', {
       name: 'test',
       path: './'
@@ -42,5 +43,83 @@ describe('sample-schematics', () => {
       .toContain(
         'private testService: TestService'
       )
+  });
+
+  it('should create component files specifying dash-separated name', () => {
+    const tree = runner.runSchematic('sample-schematics', {
+      name: 'test-sample',
+      path: './'
+    }, Tree.empty());
+
+    // assert files created
+    expect(tree.files).toEqual([
+      '/test-sample.component.html',
+      '/test-sample.component.scss',
+      '/test-sample.component.ts',
+      '/test-sample.service.ts',
+      '/test-sample.ts',
+    ]);
+
+    // assert file contents
+    expect(tree.readContent('/test-sample.component.ts'))
+      .toContain(
+        `import { TestSampleService } from './test-sample.service'`
+      )
+    expect(tree.readContent('/test-sample.component.ts'))
+      .toContain(
+        `selector: 'test-sample',
+  templateUrl: './test-sample.component.html',
+  styleUrls: ['./test-sample.component.scss']`
+      )
+    expect(tree.readContent('/test-sample.component.ts'))
+      .toContain(
+        'export class TestSampleComponent'
+      )
+    expect(tree.readContent('/test-sample.component.ts'))
+      .toContain(
+        'private testSampleService: TestSampleService'
+      )
+  });
+
+  it('should create component files specifying nested path', () => {
+    const tree = runner.runSchematic('sample-schematics', {
+      name: 'test',
+      path: './src/app/sample/test'
+    }, Tree.empty());
+
+    // assert files created
+    expect(tree.files).toEqual([
+      '/src/app/sample/test/test.component.html',
+      '/src/app/sample/test/test.component.scss',
+      '/src/app/sample/test/test.component.ts',
+      '/src/app/sample/test/test.service.ts',
+      '/src/app/sample/test/test.ts',
+    ]);
+  });
+
+  it('should fail if name is not specified', async () => {
+    let thrownError: Error | null = null;
+    try {
+      await runner.runSchematicAsync('sample-schematics', {
+        path: './'
+      }, Tree.empty()).toPromise();
+    } catch (err) {
+      thrownError = err;
+    }
+
+    expect(thrownError).toBeDefined();
+  });
+
+  it('should fail if path is not specified', async () => {
+    let thrownError: Error | null = null;
+    try {
+      await runner.runSchematicAsync('sample-schematics', {
+        name: 'test',
+      }, Tree.empty()).toPromise();
+    } catch (err) {
+      thrownError = err;
+    }
+
+    expect(thrownError).toBeDefined();
   });
 });
